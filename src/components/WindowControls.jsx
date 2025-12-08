99fb027a32bc93463620b5a0e4fd2f0fb968f559
凌岳群 <raven@lingyuequndeMacBook-Air.local>
2025-12-09 00:21:35 +0800

    implement a hoc for managing window states and add the first window - terminal

---
+++ src/components/WindowControls.jsx
@@ -0,0 +1,15 @@
+import useWindowStore from '#store/window'
+
+const WindowControls = ({ target }) => {
+  const { closeWindow } = useWindowStore()
+
+  return (
+    <div id="window-controls">
+      <div className="close" onClick={() => closeWindow(target)}></div>
+      <div className="minimize"></div>
+      <div className="maximize"></div>
+    </div>
+  )
+}
+
+export default WindowControls
