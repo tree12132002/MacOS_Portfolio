4d5d75855716d615f3044bcbe4b8d694f85a2570
Raven Ling <tree12132002@hotmail.com>
2025-12-31 00:30:14 +0800

    implement text and image window

--- src/windows/index.js
+++ src/windows/index.js
@@ -2,5 +2,7 @@ import Terminal from '#windows/Terminal'
 import Safari from '#windows/Safari'
 import Resume from '#windows/Resume'
 import Finder from '#windows/Finder'
+import Text from '#windows/Text'
+import Image from '#windows/Image'
 
-export { Terminal, Safari, Resume, Finder }
+export { Terminal, Safari, Resume, Finder, Text, Image }
